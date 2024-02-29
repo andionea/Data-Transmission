a134147758286c7b765bbe3e7b75a28c5dc6698b
Andrei Onea <andrei.onea@emeal.nttdata.com>
2024-02-29 22:25:32 +0200

    lab 1

---
+++ L1/script.js
@@ -0,0 +1,5 @@
+function execute() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    console.log(username, password);
+}
